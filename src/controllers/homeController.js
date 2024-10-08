6ba325f7c9ddb258c74d3c633e64707cb52dcc5e
hoaidzvcl <hoaixoai268@gmail.com>
2024-10-08 23:44:40 +0700

    create user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,25 @@
+const connection = require('../config/database');
+
 const homePage = (req, res) => {
     res.render('homePage');
 }
 
 const createUser = (req, res) => {
-    res.send('create ok');
-    console.log(req.body);
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+
+    console.log(email,name,city);
+
+    connection.query(
+        `INSERT INTO 
+        Users (email,name,city)
+        VALUES (?,?,?)`,
+        [email,name,city],
+        function (err, results) {
+            res.send('create sucessed');
+        }
+    );
 }
 
 module.exports = {
