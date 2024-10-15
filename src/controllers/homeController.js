b22eef104cdbbb8a4e44c61be02745355a62b0ab
hoaidzvcl <hoaixoai268@gmail.com>
2024-10-16 01:13:32 +0700

    CRUD mongoose

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,9 @@
 const connection = require('../config/database');
 const { getAllusers, createUser, getUser, updateUser, deleteUser } = require('../services/CRUDService');
+const User = require('../models/users');
 
 const homePage = async (req, res) => {
-    let results = await getAllusers();
+    let results = await User.find({});
     return res.render('homePage', { listUsers: results });
 }
 
@@ -12,7 +13,7 @@ const createPage = (req, res) => {
 
 const getUpdate = async (req, res) => {
     let userId = req.params.id;
-    let results = await getUser(userId);
+    let results = await User.findById(userId);
     res.render('update', { infoUser: results });
 }
 
@@ -22,8 +23,12 @@ const postCreateUser = async (req, res) => {
     let city = req.body.city;
 
     console.log(email, name, city);
-
-    await createUser(email, name, city);
+    await User.create({
+        email: email,
+        name: name,
+        city: city
+    });
+    // await createUser(email, name, city);
     res.redirect('/');
 }
 
@@ -33,25 +38,28 @@ const postUpdateUser = async (req, res) => {
     let name = req.body.name;
     let city = req.body.city;
 
-    await updateUser(email, name, city, userId);
+    await User.updateOne({_id: userId},{name: name, email: email, city: city});
+    // await updateUser(email, name, city, userId);
     res.redirect('/');
 }
 
 const getDelete = async (req, res) => {
     let userId = req.params.id;
-    let results = await getUser(userId);
+    // let results = await getUser(userId);
+    let results = await User.findById(userId);
     res.render('deletePage', { infoUser: results });
 }
 
 const confirmDelete = async (req, res) => {
     const userId = req.params.id;
-    const result = await deleteUser(userId);
-    if (result.success) {
-        // res.send(`Người dùng với id ${userId} đã được xóa thành công!`);
+    // const result = await deleteUser(userId);
+    const result = await User.deleteOne({_id: userId});
+    if (result.acknowledged && result.deletedCount > 0) {
         res.redirect('/');
     } else {
         res.send(`Có lỗi khi xóa người dùng với id ${userId}.`);
     }
+    
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -16,6 +16,13 @@ viewConfig(app);
 
 app.use('/', webRoutes);
 
-app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+(async () => {
+  try {
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`Example app listening on port ${port}`)
+    });
+  } catch (error) {
+    console.log(error)
+  }
+})()
